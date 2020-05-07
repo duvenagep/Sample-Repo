116480d368f21444c0b3341088c5dc21bc6e069a
Paul Duvenage <duvenagep@gmail.com>
2020-05-07 16:21:45 +0200

    Made some changes to the setup script file

--- Script.py
+++ Script.py
@@ -4,3 +4,5 @@ print(sys.version)
 print(sys.executable)
 
 print("Hello World This is the Python VS Code setup")
+
+print("This is the changes that we have made between the first and last commits")
